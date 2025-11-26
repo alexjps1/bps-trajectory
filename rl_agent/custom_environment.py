27f85a42e5eb3b5acac9a6e3d22db7d1dea49160
Moritz Schüler <moritz.schueler@stud-mail.uni-wuerzburg.de>
2025-11-26 18:20:33 +0100

    - implemented custom cnn policy - trained agent to navigate in an empty 64x64 env from a random start to a fixed goal - trained agent to navigate in a fix 64x64 env with a few obstacles from a random start to a fixed goal - started training an agent to navigate in a random env with a few obstacles from a random start to a fixed goal

---
+++ rl_agent/custom_environment.py
@@ -0,0 +1,201 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+from stable_baselines3 import A2C, PPO, DQN
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor
+import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm import tqdm
+import torch.nn as nn
+import torch
+
+class GridEnvCNN(gym.Env):
+    """Custom Environment for Grid World"""
+
+    metadata = {"render_modes": ["human"], "render_fps": 30}
+
+    def __init__(self, grid, start, goal, max_steps):
+        super().__init__()
+        self.grid = np.array(grid, dtype=np.int8)
+        self.size = self.grid.shape[0]
+        self.start = start
+        self.goal  = goal
+        self.max_steps = max_steps
+        self.current_step = 0
+        self.agent_position = list(self.start)
+        self.action_space = spaces.Discrete(4) # up, down, left, right
+        # self.observation_space = spaces.Box(low=0, high=255, shape=(3, self.size, self.size), dtype=np.uint8)
+
+        self.observation_space = spaces.Dict({
+            "image": spaces.Box(low=0, high=255, shape=(3, self.size, self.size), dtype=np.uint8),
+            "coords": spaces.Box(low=0.0, high=1.0, shape=(4,), dtype=np.float32),
+        })
+        
+
+
+    def step(self, action):
+        self.current_step += 1
+        next_position = list(self.agent_position)
+        
+        if action == 0: next_position[0] -= 1 # up
+        elif action == 1: next_position[0] += 1 # down
+        elif action == 2: next_position[1] -= 1 # left
+        elif action == 3: next_position[1] += 1 # right
+        
+        collision = False
+        if 0 <= next_position[0] < self.size and 0 <= next_position[1] < self.size and self.grid[next_position[0], next_position[1]] == 0:
+            self.agent_position = np.array(next_position, copy=True)
+        else:
+            collision = True
+
+        obs = np.zeros((3, self.size, self.size), dtype=np.uint8)
+        obs[0] = (self.grid == 1).astype(np.uint8)        # obstacles
+        obs[1, self.goal[0], self.goal[1]] = 1              # goal
+        obs[2, self.agent_position[0], self.agent_position[1]] = 1  # agent
+        obs *= 255
+
+        # observation = obs[np.newaxis, :, :] # shape to (1, Size, Size)
+        terminated = np.array_equal(self.agent_position, self.goal)
+        truncated = self.current_step >= self.max_steps
+
+        reward = 0
+        if terminated:
+            reward += 10
+        elif collision:
+            reward -= 0.1
+        else:
+            reward -= 0.01
+        
+        info = {}
+        ay, ax = self.agent_position
+        gy, gx = self.goal
+        coords = np.array([ay, ax, gy, gx], dtype=np.float32) / self.size # normalize
+
+        return {'image': obs, 'coords': coords}, reward, terminated, truncated, info
+
+
+    def reset(self, seed=None, options=None):
+        obs = np.zeros((3, self.size, self.size), dtype=np.uint8)
+        obs[0] = (self.grid == 1).astype(np.uint8)        # obstacles
+        obs[1, self.goal[0], self.goal[1]] = 1              # goal
+        obs[2, self.start[0], self.start[1]] = 1  # agent  # agent
+        obs *= 255
+
+        # observation = obs[np.newaxis, :, :] # shape to (1, Size, Size)
+        self.agent_position = list(self.start)
+        self.current_step = 0
+        info = {}
+        
+        ay, ax = self.agent_position
+        gy, gx = self.goal
+        coords = np.array([ay, ax, gy, gx], dtype=np.float32) / self.size # normalize
+        
+        return {'image': obs, 'coords': coords}, info
+
+    def render(self):
+        obs = np.zeros((3, self.size, self.size), dtype=np.uint8)
+        obs[0] = (self.grid == 1).astype(np.uint8)        # obstacles
+        obs[1, self.goal[0], self.goal[1]] = 1              # goal
+        obs[2, self.agent_position[0], self.agent_position[1]] = 1  # agent
+        obs *= 255
+
+        observation = obs
+
+        symbols = {0: '.', 1: '#', 2: 'A', 3: 'G'}
+        print("\n".join(" ".join(symbols.get(v, '?') for v in row) for row in observation))
+        print()
+
+
+
+
+class GridEnvMLP(gym.Env):
+    """Custom Environment for Grid World"""
+
+    metadata = {"render_modes": ["human"], "render_fps": 30}
+
+    def __init__(self, grid, start, goal, max_steps):
+        super().__init__()
+        self.grid = np.array(grid, dtype=np.int8)
+        self.size = self.grid.shape[0]
+        self.start = start
+        self.goal  = goal
+        self.max_steps = max_steps
+        self.current_step = 0
+        self.agent_position = list(self.start)
+        self.action_space = spaces.Discrete(4) # up, down, left, right
+        self.observation_space = spaces.Box(low=0, high=3, shape=(1, self.size, self.size), dtype=np.uint8) # 0: free space, 1: obstacle, 2: agent, 3: goal
+        
+
+
+    def step(self, action):
+        self.current_step += 1
+        next_position = list(self.agent_position)
+        prev_distance = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal))
+        
+        if action == 0: next_position[0] -= 1 # up
+        elif action == 1: next_position[0] += 1 # down
+        elif action == 2: next_position[1] -= 1 # left
+        elif action == 3: next_position[1] += 1 # right
+        
+        collision = False
+        if 0 <= next_position[0] < self.size and 0 <= next_position[1] < self.size and self.grid[next_position[0], next_position[1]] == 0:
+            self.agent_position = np.array(next_position, copy=True)
+        else:
+            collision = True
+
+        obs = np.zeros((3, self.size, self.size), dtype=np.uint8)
+        obs = (self.grid == 1).astype(np.uint8)        # obstacles
+        obs[self.goal[0], self.goal[1]] = 3              # goal
+        obs[self.agent_position[0], self.agent_position[1]] = 2  # agent
+        obs = obs[np.newaxis, :, :]
+
+        # observation = obs[np.newaxis, :, :] # shape to (1, Size, Size)
+        terminated = np.array_equal(self.agent_position, self.goal)
+        truncated = self.current_step >= self.max_steps
+
+        reward = 0
+        if terminated:
+            reward += 10
+        elif collision:
+            reward -= 0.1
+        else:
+            reward -= 0.01
+
+        next_distance = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal))
+        distance = prev_distance - next_distance
+        # reward += distance * 0.01  # reward getting closer
+        
+        info = {}
+
+        return obs, reward, terminated, truncated, info
+
+
+    def reset(self, seed=None, options=None):
+        obs = np.zeros((3, self.size, self.size), dtype=np.uint8)
+        obs = (self.grid == 1).astype(np.uint8)        # obstacles
+        obs[self.goal[0], self.goal[1]] = 3              # goal
+        obs[self.start[0], self.start[1]] = 2  # agent  # agent
+        obs = obs[np.newaxis, :, :]
+
+        # observation = obs[np.newaxis, :, :] # shape to (1, Size, Size)
+        self.agent_position = list(self.start)
+        self.current_step = 0
+        info = {}
+        
+        return obs, info
+
+    def render(self):
+        obs = np.zeros((3, self.size, self.size), dtype=np.uint8)
+        obs = (self.grid == 1).astype(np.uint8)        # obstacles
+        obs[self.goal[0], self.goal[1]] = 3             # goal
+        obs[self.agent_position[0], self.agent_position[1]] = 2  # agent
+        obs = obs[np.newaxis, :, :]
+
+        observation = obs
+
+        symbols = {0: '.', 1: '#', 2: 'A', 3: 'G'}
+        print("\n".join(" ".join(symbols.get(v, '?') for v in row) for row in observation))
+        print()
---
+++ rl_agent/custom_policy.py
@@ -0,0 +1,69 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+from stable_baselines3 import A2C, PPO, DQN
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor
+import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm import tqdm
+import torch.nn as nn
+import torch
+
+
+class CustomCNN(BaseFeaturesExtractor):
+
+    def __init__(self, observation_space: gym.spaces.Dict, features_dim: int = 256):
+        super(CustomCNN, self).__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        # Re-ordering will be done by pre-preprocessing or wrapper
+
+        image_observation_space = observation_space['image']
+        coords_observation_space = observation_space['coords']
+
+        # image input
+        n_input_channels = image_observation_space.shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(
+                torch.as_tensor(image_observation_space.sample()[None]).float()
+            ).shape[1]
+
+        # coordinate input
+        self.l1 = nn.Sequential(
+            nn.Linear(4, 4), nn.ReLU()
+        )
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten + 4, features_dim), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        image_observations = observations['image']
+        coords_observations = observations['coords']
+
+        cnn_output = self.cnn(image_observations)
+        f1_output = self.l1(coords_observations)
+        
+        return self.linear(torch.cat([cnn_output, f1_output], dim = 1))
+    
+
+
+class CustomCNNPolicy(ActorCriticPolicy):
+    def __init__(self, observation_space, action_space, lr_schedule, **kwargs):
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            features_extractor_class=CustomCNN,  #custom extractor
+            features_extractor_kwargs=dict(features_dim=256), 
+            **kwargs
+        )
