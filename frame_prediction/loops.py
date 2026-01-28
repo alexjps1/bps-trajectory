97bb2d77df75c15ce5d4a2e673a1534df6d1d988
Alexander João Peterson Santos <git@alexjps.com>
2026-01-28 17:06:07 +0100

    feat: F1 and Accuracy calculation in eval func
    To give better stdout output and work together with the visualization
    module (not yet integrated), did some refactoring of how the train and
    eval functions handle calculating and returning loss metrics, including
    adding F1 and accuracy to the eval script and returning dicts instead of
    tuples / single values.

--- frame_prediction/loops.py
+++ frame_prediction/loops.py
@@ -8,7 +8,7 @@ Moritz Schüler and Alexander João Peterson Santos
 # standard library imports
 import os
 from pathlib import Path
-from typing import cast
+from typing import cast, Dict
 
 # third party imports
 import matplotlib.pyplot as plt
@@ -25,7 +25,7 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from tqdm import tqdm
 
 # constants
-GPU_VRAM_LOGGING = True
+GPU_VRAM_LOGGING = False
 THIS_DIR = Path(__file__).resolve().parent
 
 
@@ -60,7 +60,7 @@ def train_scene_to_scene(
     run_path: Path,
     trial=None,
     writer: SummaryWriter | None = None,
-) -> float:
+) -> Dict[str, float]:
     """
     Train a scene-to-scene frame prediction model.
     Logs MSE and BCE loss but optimizes on BCE.
@@ -106,17 +106,28 @@ def train_scene_to_scene(
     os.makedirs(run_path, exist_ok=True)
 
     # checkpoint setup
-    best_val_loss: float = float("inf")
     improvement_threshold: float = 0.01
     checkpoint_path: Path = run_path / "checkpoints"
     os.makedirs(checkpoint_path, exist_ok=True)
 
     epochs_between_evals = max(1, epochs_between_evals)
 
+    best_val_bce_loss: float = float("inf")
+    avg_train_bce_loss: float = float("inf")
+    avg_train_bce_loss_bin: float = float("inf")
+    avg_train_mse_loss: float = float("inf")
+    avg_train_mse_loss_bin: float = float("inf")
+
     # training loop
+    if num_epochs <= 0:
+        raise ValueError(f"Training loop called with num_epochs={num_epochs}")
     for epoch in range(num_epochs):
         model.train()
-        loop: tqdm = tqdm(train_dataloader, desc=f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}]", ncols=80)
+        loop: tqdm = tqdm(
+            train_dataloader,
+            desc=f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}]",
+            ncols=80,
+        )
 
         epoch_bce_loss: float = 0.0
         epoch_bce_loss_bin: float = 0.0
@@ -148,14 +159,24 @@ def train_scene_to_scene(
                 predicted_frames = model.forward(input_frames)
             else:
                 # Use multi-step for predicting multiple frames autoregressively
-                predicted_frames = model.forward_multi_step(input_frames, num_target_frames)
+                predicted_frames = model.forward_multi_step(
+                    input_frames, num_target_frames
+                )
             predicted_frames_bin: torch.Tensor = (predicted_frames >= 0.5).float()
 
             # loss calculation over all predicted frames
-            batch_bce: torch.Tensor = bce_criterion(predicted_frames, target_frames, reduction="mean")
-            batch_bce_bin: torch.Tensor = bce_criterion(predicted_frames_bin, target_frames, reduction="mean")
-            batch_mse: torch.Tensor = mse_criterion(predicted_frames, target_frames, reduction="mean")
-            batch_mse_bin: torch.Tensor = mse_criterion(predicted_frames_bin, target_frames, reduction="mean")
+            batch_bce: torch.Tensor = bce_criterion(
+                predicted_frames, target_frames, reduction="mean"
+            )
+            batch_bce_bin: torch.Tensor = bce_criterion(
+                predicted_frames_bin, target_frames, reduction="mean"
+            )
+            batch_mse: torch.Tensor = mse_criterion(
+                predicted_frames, target_frames, reduction="mean"
+            )
+            batch_mse_bin: torch.Tensor = mse_criterion(
+                predicted_frames_bin, target_frames, reduction="mean"
+            )
 
             # backward pass and optimization (optimizing BCE)
             batch_bce.backward()
@@ -191,8 +212,14 @@ def train_scene_to_scene(
             writer.add_scalar("Train/MSE", avg_train_mse_loss, epoch)
             writer.add_scalar("Train/MSE_bin", avg_train_mse_loss_bin, epoch)
 
-        if epoch % epochs_between_evals == 0 or epoch == num_epochs - 1:
-            avg_val_bce_loss, avg_val_bce_loss_bin, avg_val_mse_loss, avg_val_mse_loss_bin = evaluate_scene_to_scene(
+        if epoch % epochs_between_evals != 0 and epoch != num_epochs - 1:
+            # no evaluation for this epoch
+            print(
+                f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}] | Train | BCE: {avg_train_bce_loss:.6f} | BCE bin: {avg_train_bce_loss_bin:.6f} | MSE: {avg_train_mse_loss:.6f} | MSE bin: {avg_train_mse_loss_bin:.6f}"
+            )
+        else:
+            # perform an evaluation
+            eval_summary_dict: Dict[str, float] = evaluate_scene_to_scene(
                 model,
                 val_dataloader,
                 num_target_frames,
@@ -203,36 +230,65 @@ def train_scene_to_scene(
                 epoch=epoch,
                 writer=writer,
             )
+            # unpack values from the evaluation
+            avg_val_bce_loss = eval_summary_dict["avg_val_bce"]
+            avg_val_bce_loss_bin = eval_summary_dict["avg_val_bce_bin"]
+            avg_val_mse_loss = eval_summary_dict["avg_val_mse"]
+            avg_val_mse_loss_bin = eval_summary_dict["avg_val_mse_bin"]
+            avg_val_f1 = eval_summary_dict["avg_val_f1"]
+            avg_val_accuracy = eval_summary_dict["avg_val_accuracy"]
+
             print(
                 f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}] | Train | BCE: {avg_train_bce_loss:.6f} | BCE bin: {avg_train_bce_loss_bin:.6f} | MSE: {avg_train_mse_loss:.6f} | MSE bin: {avg_train_mse_loss_bin:.6f}\n"
-                f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}] | Val   | BCE: {avg_val_bce_loss:.6f} | BCE bin: {avg_val_bce_loss_bin:.6f} | MSE: {avg_val_mse_loss:.6f} | MSE bin: {avg_val_mse_loss_bin:.6f}\n"
+                f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}] | Val   | BCE: {avg_val_bce_loss:.6f} | BCE bin: {avg_val_bce_loss_bin:.6f} | MSE: {avg_val_mse_loss:.6f} | MSE bin: {avg_val_mse_loss_bin:.6f} | F1: {avg_val_f1:.6f} | Acc: {avg_val_accuracy:.6f}\n"
             )
 
+            # write metrics to tensorboard
             if writer is not None:
                 writer.add_scalar("Val/BCE", avg_val_bce_loss, epoch)
                 writer.add_scalar("Val/BCE_bin", avg_val_bce_loss_bin, epoch)
                 writer.add_scalar("Val/MSE", avg_val_mse_loss, epoch)
                 writer.add_scalar("Val/MSE_bin", avg_val_mse_loss_bin, epoch)
+                writer.add_scalar("Val/F1", avg_val_f1, epoch)
+                writer.add_scalar("Val/Accuracy", avg_val_accuracy, epoch)
 
             # checkpoint on evaluation epochs if improved
-            if avg_val_bce_loss < best_val_loss - improvement_threshold:
+            if avg_val_bce_loss < best_val_bce_loss - improvement_threshold:
                 print(
-                    f"              Validation BCE Loss improved from {best_val_loss:.6f} to {avg_val_bce_loss:.6f}. Saving..."
+                    f"              Validation BCE Loss improved from {best_val_bce_loss:.6f} to {avg_val_bce_loss:.6f}. Saving..."
                 )
-                best_val_loss = avg_val_bce_loss
                 save_filename: str = f"{run_name}_e{epoch:02d}.pt"
                 save_filepath: Path = checkpoint_path / save_filename
-                save_checkpoint(model, epoch, optimizer, best_val_loss, save_filepath)
+                save_checkpoint(
+                    model, epoch, optimizer, avg_val_bce_loss, save_filepath
+                )
+
             if trial is not None:
                 trial.report(avg_val_bce_loss, epoch)
                 if trial.should_prune():
                     raise optuna.TrialPruned()
-        else:
-            print(
-                f"[Epoch {epoch:02d}/{(num_epochs - 1):02d}] | Train | BCE: {avg_train_bce_loss:.6f} | BCE bin: {avg_train_bce_loss_bin:.6f} | MSE: {avg_train_mse_loss:.6f} | MSE bin: {avg_train_mse_loss_bin:.6f}"
-            )
 
-    return best_val_loss
+            # update the best_val_loss to help optuna optimize
+            best_val_bce_loss = min(best_val_bce_loss, avg_val_bce_loss)
+
+    if float("inf") in [
+        best_val_bce_loss,
+        avg_train_bce_loss,
+        avg_train_bce_loss_bin,
+        avg_train_mse_loss,
+        avg_train_mse_loss_bin,
+    ]:
+        raise ValueError(
+            "Infinite loss found in one of the loss measures. There was a problem with the training loop."
+        )
+
+    return {
+        "best_val_bce_loss": best_val_bce_loss,  # bce loss is used by the parameter optimizer (Adam) and by hyperparameter optimizer (Optuna)
+        "final_train_bce_loss": avg_train_bce_loss,
+        "final_train_bce_loss_bin": avg_train_bce_loss_bin,
+        "final_train_mse_loss": avg_train_mse_loss,
+        "final_train_mse_loss_bin": avg_train_mse_loss_bin,
+    }
 
 
 def evaluate_scene_to_scene(
@@ -245,7 +301,7 @@ def evaluate_scene_to_scene(
     max_visualizations: int = 0,
     epoch: int | None = None,
     writer: SummaryWriter | None = None,
-) -> tuple[float, float, float, float]:
+) -> Dict[str, float]:
     """
     Evaluate a scene-to-scene frame prediction model.
 
@@ -279,6 +335,8 @@ def evaluate_scene_to_scene(
     total_bce_loss_bin: float = 0.0
     total_mse_loss: float = 0.0
     total_mse_loss_bin: float = 0.0
+    total_f1_score: float = 0.0
+    total_accuracy: float = 0.0
 
     bce_criterion = F.binary_cross_entropy
     mse_criterion = F.mse_loss
@@ -302,15 +360,38 @@ def evaluate_scene_to_scene(
             if num_target_frames == 1:
                 predicted_frames = model.forward(input_frames)
             else:
-                predicted_frames = model.forward_multi_step(input_frames, num_target_frames)
+                predicted_frames = model.forward_multi_step(
+                    input_frames, num_target_frames
+                )
             predicted_frames_bin: torch.Tensor = (predicted_frames >= 0.5).float()
+
+            # --- Metrics Calculation ---
+            # Ensure targets are binary for metrics
+            target_frames_bin = (target_frames >= 0.5).float()
+
+            # Accuracy
+            batch_accuracy = (predicted_frames_bin == target_frames_bin).float().mean()
+
+            # F1 Score
+            tp = (predicted_frames_bin * target_frames_bin).sum()
+            fp = (predicted_frames_bin * (1 - target_frames_bin)).sum()
+            fn = ((1 - predicted_frames_bin) * target_frames_bin).sum()
+
+            batch_f1 = 2 * tp / (2 * tp + fp + fn + 1e-7)
+
+            total_accuracy += batch_accuracy.item()
+            total_f1_score += batch_f1.item()
+            # ---------------------------
+
             if should_visualize and saved_visualizations < max_visualizations:
                 batch_size = predicted_frames.size(0)
                 for batch_idx in range(batch_size):
                     if saved_visualizations >= max_visualizations:
                         break
                     if epoch is not None:
-                        image_filename = f"{run_name}_e{epoch:03d}_img{saved_visualizations:02d}.png"
+                        image_filename = (
+                            f"{run_name}_e{epoch:03d}_img{saved_visualizations:02d}.png"
+                        )
                     else:
                         image_filename = f"{run_name}_img{saved_visualizations:02d}.png"
                     image_path = cast(Path, images_path) / image_filename
@@ -338,10 +419,18 @@ def evaluate_scene_to_scene(
 
                     saved_visualizations += 1
 
-            batch_bce_loss: torch.Tensor = bce_criterion(predicted_frames, target_frames, reduction="mean")
-            batch_bce_loss_bin: torch.Tensor = bce_criterion(predicted_frames_bin, target_frames, reduction="mean")
-            batch_mse_loss: torch.Tensor = mse_criterion(predicted_frames, target_frames, reduction="mean")
-            batch_mse_loss_bin: torch.Tensor = mse_criterion(predicted_frames_bin, target_frames, reduction="mean")
+            batch_bce_loss: torch.Tensor = bce_criterion(
+                predicted_frames, target_frames, reduction="mean"
+            )
+            batch_bce_loss_bin: torch.Tensor = bce_criterion(
+                predicted_frames_bin, target_frames, reduction="mean"
+            )
+            batch_mse_loss: torch.Tensor = mse_criterion(
+                predicted_frames, target_frames, reduction="mean"
+            )
+            batch_mse_loss_bin: torch.Tensor = mse_criterion(
+                predicted_frames_bin, target_frames, reduction="mean"
+            )
 
             total_bce_loss += batch_bce_loss.item()
             total_bce_loss_bin += batch_bce_loss_bin.item()
@@ -350,9 +439,18 @@ def evaluate_scene_to_scene(
 
     model.train()
 
-    avg_bce = total_bce_loss / len(dataloader)
-    avg_bce_bin = total_bce_loss_bin / len(dataloader)
-    avg_mse = total_mse_loss / len(dataloader)
-    avg_mse_bin = total_mse_loss_bin / len(dataloader)
+    avg_val_bce = total_bce_loss / len(dataloader)
+    avg_val_bce_bin = total_bce_loss_bin / len(dataloader)
+    avg_val_mse = total_mse_loss / len(dataloader)
+    avg_val_mse_bin = total_mse_loss_bin / len(dataloader)
+    avg_val_f1 = total_f1_score / len(dataloader)
+    avg_val_accuracy = total_accuracy / len(dataloader)
 
-    return avg_bce, avg_bce_bin, avg_mse, avg_mse_bin
+    return {
+        "avg_val_bce": avg_val_bce,
+        "avg_val_bce_bin": avg_val_bce_bin,
+        "avg_val_mse": avg_val_mse,
+        "avg_val_mse_bin": avg_val_mse_bin,
+        "avg_val_f1": avg_val_f1,
+        "avg_val_accuracy": avg_val_accuracy,
+    }
